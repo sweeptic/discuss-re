b719963a430d9fe37ef22ef3e9eef805aaeee0ad
Attila Horvath <sweeptic@gmail.com>
2025-05-29 12:55:43 +0200

    11. Sign In, Sign out, and Checking Auth Status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 import * as actions from '@/actions';
+import { auth } from '@/auth';
+import Profile from '@/components/profile';
 import { Button } from '@heroui/button';
 
 export default async function Home() {
-  //   const session = await auth();
+  const session = await auth();
 
   return (
     <div>
@@ -14,9 +16,9 @@ export default async function Home() {
         <Button type="submit">Sign Out</Button>
       </form>
 
-      {/* {session?.user ? <div>Signed In {JSON.stringify(session.user)}</div> : <div>Signed Out</div>} */}
+      {session?.user ? <div>Signed In {JSON.stringify(session.user)}</div> : <div>Signed Out</div>}
 
-      {/* <Profile /> */}
+      <Profile />
     </div>
   );
 }
---
+++ src/components/profile.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import { useSession } from 'next-auth/react';
+
+export default function Profile() {
+  const session = useSession();
+
+  console.log('session', session);
+
+  return (
+    <div>
+      From useSession: {JSON.stringify(session.data?.user)}user is {session.data?.user ? '' : 'NOT'} signed in
+    </div>
+  );
+}
