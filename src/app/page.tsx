daa23837f748824d5195c3acd069c65ca68b738a
Attila Horvath <sweeptic@gmail.com>
2025-05-29 10:18:03 +0200

    11. Sign In, Sign out, and Checking Auth Status

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,22 @@
-export default function Home() {
-  return <div>hello</div>;
+import * as actions from '@/actions';
+import { Button } from '@heroui/button';
+
+export default async function Home() {
+  //   const session = await auth();
+
+  return (
+    <div>
+      <form action={actions.signIn}>
+        <Button type="submit">Sign In</Button>
+      </form>
+
+      <form action={actions.signOut}>
+        <Button type="submit">Sign Out</Button>
+      </form>
+
+      {/* {session?.user ? <div>Signed In {JSON.stringify(session.user)}</div> : <div>Signed Out</div>} */}
+
+      {/* <Profile /> */}
+    </div>
+  );
 }
